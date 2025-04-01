2afba37b0c231cffa7417dbad25789474a477a25
Harmony Lumumba Muyagane <lumumbaharmony@gmail.com>
2025-04-01 16:56:21 +0300

    Update vite.config.js

--- react-app/vite.config.js
+++ react-app/vite.config.js
@@ -6,5 +6,15 @@ export default defineConfig({
   plugins: [
     react(),
   ],
-
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        headers: {
+          Accept: 'application/json',
+        },
+      },
+    },
+  },
 })
